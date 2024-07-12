13f0d1608cda3eb0507feddea9f8ace01fb6df76
CookyNdi <73926548+CookyNdi@users.noreply.github.com>
2024-07-12 09:32:23 +0700

    feat : navbar content & responsive

--- src/components/layout/navbar/navbar.tsx
+++ src/components/layout/navbar/navbar.tsx
@@ -1,10 +1,18 @@
+import Link from "next/link";
+
 import { ToggleTheme } from "@/components/theme/toggle-theme";
+import Profile from "../profile/profile";
 
 export default function Navbar() {
   return (
-    <div className="fixed top-0 z-[110] flex h-[60px] w-full items-center justify-between border-b bg-background/50 px-12 backdrop-blur-md">
-      <h1>Taskpad</h1>
-      <ToggleTheme />
+    <div className="fixed top-0 z-[110] flex h-[60px] w-full items-center justify-between border-b bg-background/50 px-4 backdrop-blur-md lg:px-12">
+      <Link href={"/"} className="text-2xl font-semibold">
+        Taskpad
+      </Link>
+      <div className="flex items-center gap-x-4">
+        <ToggleTheme />
+        <Profile />
+      </div>
     </div>
   );
 }
---
+++ src/components/layout/profile/profile.tsx
@@ -0,0 +1,35 @@
+import { Avatar, AvatarFallback, AvatarImage } from "@/components/ui/avatar";
+import {
+  DropdownMenu,
+  DropdownMenuContent,
+  DropdownMenuItem,
+  DropdownMenuLabel,
+  DropdownMenuSeparator,
+  DropdownMenuTrigger,
+} from "@/components/ui/dropdown-menu";
+import Link from "next/link";
+
+export default function Profile() {
+  return (
+    <div>
+      <DropdownMenu>
+        <DropdownMenuTrigger asChild>
+          <Avatar className="cursor-pointer">
+            <AvatarImage src="https://github.com/shadcn.png" />
+            <AvatarFallback>CN</AvatarFallback>
+          </Avatar>
+        </DropdownMenuTrigger>
+        <DropdownMenuContent className="z-[120] w-56" align="end">
+          <DropdownMenuLabel>My Account</DropdownMenuLabel>
+          <DropdownMenuSeparator />
+          <Link href={"/p/CookyNdi"}>
+            <DropdownMenuItem className="cursor-pointer">
+              Profile
+            </DropdownMenuItem>
+          </Link>
+          <DropdownMenuItem className="cursor-pointer">Logout</DropdownMenuItem>
+        </DropdownMenuContent>
+      </DropdownMenu>
+    </div>
+  );
+}
